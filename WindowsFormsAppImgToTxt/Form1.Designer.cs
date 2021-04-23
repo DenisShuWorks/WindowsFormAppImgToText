3d474198dced0fd58382741bbdb84ab8a5c2141c
DenisShuWorks <denshufly@mail.ru>
2021-04-23 10:42:28 +0600

    Done

---
+++ WindowsFormsAppImgToTxt/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WindowsFormsAppImgToTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox.Location = new System.Drawing.Point(25, 82);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(265, 250);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(471, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Language";
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Items.AddRange(new object[] {
+            "Arabic",
+            "ChineseSimplified",
+            "ChineseTraditional ",
+            "Czech",
+            "Danish",
+            "Dutch",
+            "English",
+            "Finnish",
+            "French",
+            "German",
+            "Greek ",
+            "Hungarian",
+            "Italian",
+            "Japanese",
+            "Korean  ",
+            "Norwegian",
+            "Polish",
+            "Portuguese",
+            "Russian ",
+            "Spanish",
+            "Swedish ",
+            "Turkish"});
+            this.cmbLanguage.Location = new System.Drawing.Point(532, 55);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(124, 21);
+            this.cmbLanguage.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(337, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 61);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Recognize";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(474, 82);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(304, 250);
+            this.txtResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbLanguage);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Image to Text";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbLanguage;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ WindowsFormsAppImgToTxt/Form1.cs
@@ -0,0 +1,198 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsAppImgToTxt.Models;
+
+namespace WindowsFormsAppImgToTxt
+{
+    public partial class Form1 : Form
+    {
+
+        public string ImagePath { get; set; }
+        public string PdfPath { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbLanguage.SelectedIndex = 6;//English
+        }
+
+        private string getSelectedLanguage()
+        {
+            string strLang = "";
+            switch (cmbLanguage.SelectedIndex)
+            {
+                case 0:
+                    strLang = "ara";
+                    break;
+
+                case 1:
+                    strLang = "chs";
+                    break;
+
+                case 2:
+                    strLang = "cht";
+                    break;
+                case 3:
+                    strLang = "cze";
+                    break;
+                case 4:
+                    strLang = "dan";
+                    break;
+                case 5:
+                    strLang = "dut";
+                    break;
+                case 6:
+                    strLang = "eng";
+                    break;
+                case 7:
+                    strLang = "fin";
+                    break;
+                case 8:
+                    strLang = "fre";
+                    break;
+                case 9:
+                    strLang = "ger";
+                    break;
+                case 10:
+                    strLang = "gre";
+                    break;
+                case 11:
+                    strLang = "hun";
+                    break;
+                case 12:
+                    strLang = "jap";
+                    break;
+                case 13:
+                    strLang = "kor";
+                    break;
+                case 14:
+                    strLang = "nor";
+                    break;
+                case 15:
+                    strLang = "pol";
+                    break;
+                case 16:
+                    strLang = "por";
+                    break;
+                case 17:
+                    strLang = "spa";
+                    break;
+                case 18:
+                    strLang = "swe";
+                    break;
+                case 19:
+                    strLang = "tur";
+                    break;
+
+            }
+            return strLang;
+
+        }
+
+        private byte[] ImageToBase64(Image image, System.Drawing.Imaging.ImageFormat format)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                return imageBytes;
+            }
+        }
+
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            PdfPath = ImagePath = ""; pictureBox.BackgroundImage = null;
+            OpenFileDialog fileDlg = new OpenFileDialog();
+            fileDlg.Filter = "jpeg and png files|*.png;*.jpg;*.JPG";
+            if (fileDlg.ShowDialog() == DialogResult.OK)
+            {
+                FileInfo fileInfo = new FileInfo(fileDlg.FileName);
+                if (fileInfo.Length > 5 * 1024 * 1024)
+                {
+                    //Size limit depends: Free API 1 MB, PRO API 5 MB and more
+                    MessageBox.Show("Image file size limit reached (1MB free API)");
+                    return;
+                }
+                pictureBox.BackgroundImage = Image.FromFile(fileDlg.FileName);
+                ImagePath = fileDlg.FileName;
+            }
+        }
+
+        private async void button2_Click_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ImagePath) && string.IsNullOrEmpty(PdfPath))
+                return;
+
+            txtResult.Text = "";
+
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.Timeout = new TimeSpan(1, 1, 1);
+
+
+                MultipartFormDataContent form = new MultipartFormDataContent();
+                form.Add(new StringContent("helloworld"), "apikey");
+                form.Add(new StringContent(getSelectedLanguage()), "language");
+
+                form.Add(new StringContent("2"), "ocrengine");
+                form.Add(new StringContent("true"), "scale");
+                form.Add(new StringContent("true"), "istable");
+
+                if (string.IsNullOrEmpty(ImagePath) == false)
+                {
+                    byte[] imageData = File.ReadAllBytes(ImagePath);
+                    form.Add(new ByteArrayContent(imageData, 0, imageData.Length), "image", "image.jpg");
+                }
+
+                HttpResponseMessage response = await httpClient.PostAsync("https://api.ocr.space/Parse/Image", form);
+
+                string strContent = await response.Content.ReadAsStringAsync();
+
+
+
+                Rootobject ocrResult = JsonConvert.DeserializeObject<Rootobject>(strContent);
+
+
+                if (ocrResult.OCRExitCode == 1)
+                {
+                    for (int i = 0; i < ocrResult.ParsedResults.Count(); i++)
+                    {
+                        txtResult.Text = txtResult.Text + ocrResult.ParsedResults[i].ParsedText;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("ERROR: " + strContent);
+                }
+
+
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Ошибка! Повторите попытку или измените параметры.");
+            }
+
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+    }
+}
